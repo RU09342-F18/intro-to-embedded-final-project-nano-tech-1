ffaf6787d7800551096defb8afcfa72096e2dde0
Tyler McLemore <mclemoret0@students.rowan.edu>
2018-12-05 13:47:58 -0500

    Added interupt for P1.3

--- main.c
+++ main.c
@@ -1,7 +1,34 @@
+//Includes
 #include <msp430.h>
 #include <Setup.h>
+//Functions
+void MotionSensor();	
 
 
 int main(void){
  
+ 
+ UARTSetup(); //Setup UART
+ 
+ TimerSetup(); //Setup Timers
+ 
+ 
+}
+
+
+void MotionSensor(){
+	
+}
+
+
+
+//--------------------Interupts--------------------
+
+//This should be a working interupt for port 1.3
+#pragma vector=PORT1_VECTOR
+__interrupt void Port_1(void)
+
+{
+  P1OUT ^= BIT0;                            // P1.0 = toggle
+  P1IFG &= ~BIT3;                           // P1.3 IFG cleared
 }
