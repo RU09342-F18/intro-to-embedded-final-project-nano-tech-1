a8a00a32907f1f368d1d2b8fa78695ac13829a51
Tyler McLemore <mclemoret0@students.rowan.edu>
2018-12-05 14:57:12 -0500

    Added comments to interrupt

--- main.c
+++ main.c
@@ -37,4 +37,9 @@ __interrupt void Port_1(void)
 {
   P1OUT ^= BIT0;                            // P1.0 = toggle
   P1IFG &= ~BIT3;                           // P1.3 IFG cleared
+  
+  /*
+  This interrupt should fire when the morion sensor sees something
+  When the motion sensor sees something the buzzer should go off and the LED should blink
+  */
 }
