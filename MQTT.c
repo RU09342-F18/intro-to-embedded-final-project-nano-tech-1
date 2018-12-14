a59887128a45e17343220d5032e3980d706c46fe
Tyler McLemore <mclemoret0@students.rowan.edu>
2018-12-14 16:52:40 -0500

    Implemented basics for Publish functions

--- MQTT.c
+++ MQTT.c
@@ -15,22 +15,35 @@
   ~Unsubscribe      SYNTAX: "~Topic\n"
   */
   
-	char PublishBuzzer(){
-		unsigned int count = 0;
-		//char Publish[10]={'$','S','e','c','u','r','i','t','y','\0'};
-		while (!(IFG2&UCA0TXIFG) && count <= 8){	                // USCI_A0 TX buffer ready?
-		UCA0TXBUF = Publish[count];
+	char PublishBuzzer(unsigned char value){
+		char Buzzer [12] = {'#','S','e','c','u','r','i','t','y','B','x','\n'}
+		int count = 0;
+		Buzzer [10] = value;
+		while (!(IFG2&UCA0TXIFG) && count <= 11){	                // USCI_A0 TX buffer ready?
+		UCA0TXBUF = Buzzer[count];
 		count++;
+		}
   }
-  count = 0;
+
+	
+	char PublishMotion(unsigned char value){
+		char Motion [11] = {'#','S','e','c','u','r','i','t','y','M','x','\n'}
+		int count = 0;
+		Motion [10] = value;
+		while (!(IFG2&UCA0TXIFG) && count <= 11){	                // USCI_A0 TX buffer ready?
+		UCA0TXBUF = Motion[count];
+		count++;
+		}
 	}
 	
-	char PublishMotion(){
-		
-	}
-	
-	char PublishSound(){
-		
+	char PublishSound(unsigned char value){
+		char Sound [11] = {'#','S','e','c','u','r','i','t','y','S','x','\n'}
+		int count = 0;
+		Sound [10] = value;
+		while (!(IFG2&UCA0TXIFG) && count <= 11){	                // USCI_A0 TX buffer ready?
+		UCA0TXBUF = Sound[count];
+		count++;
+		}
 	}
 
 	void ReceivedMessage(){
